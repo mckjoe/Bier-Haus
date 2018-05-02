5e1cce2583bd961b170aed13d336367eb223b181
joe mckinney and semhar kidane <joe@>
2018-05-02 10:31:24 -0700

    add Keg class to models and import keg class to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Keg } from './model/tap-room.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tap Room';
 }
---
+++ src/app/model/tap-room.model.ts
@@ -0,0 +1,4 @@
+export class Keg {
+  public pints: number = 124;
+  constructor(public name: string, public brand: string, public abv: number, public price: number) {}
+}

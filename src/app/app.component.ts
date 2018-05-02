4ae4af6fa3505f72a0f938dac1aafffdaab70746
joe mckinney and semhar kidane <joe@>
2018-05-02 11:01:45 -0700

    add hard coded keg options to the html as list elements and style the list elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Keg } from './model/tap-room.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tap Room';
+  title = 'Bier Haus';
+  kegs: Keg[] = [
+    new Keg("Na Zdravi Pils", "Southern Oregon", 4.8, 5),
+    new Keg("Kolsch", "Occidental", 4.4, 5),
+    new Keg("Sterling Pils", "Ninkasi", 5.1, 6),
+    new Keg("Omission Lager", "Widmer Bros.", 4.6, 5),
+    new Keg("River Ale", "Deschutes", 4, 5),
+    new Keg("Chainbreaker White IPA", "Deschutes", 5.6, 6),
+  ];
+  
 }
